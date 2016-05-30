3998fb07a3f7206b57bb3c562901a6444b367614
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-05-30 17:55:31 +0300

    activated authorization

--- SEproject-backend/rest-module/src/main/java/ro/ucv/ace/filter/CORSFilter.java
+++ SEproject-backend/rest-module/src/main/java/ro/ucv/ace/filter/CORSFilter.java
@@ -19,7 +19,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
