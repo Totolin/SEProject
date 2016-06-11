c77a7527417895813199f5a955fab79e8c683334
Cristian Stefan TOTOLIN <ctotolin@bitdefender.com>
2016-06-11 14:40:31 +0300

    Updated tests

--- SEproject-tests/api.spec.js
+++ SEproject-tests/api.spec.js
@@ -20,7 +20,13 @@ var admin = {
     password : 'admin'
 }
 
+var secretary = {
+    username : 'diana.micu',
+    password : 'secretary'
+}
+
 var secretaries = {
+    auth: null,
     professors : {
         types : {
             "address": String,
@@ -87,11 +93,12 @@ var secretaries = {
         },
 
         right : {
-            "day": 2,
+            "day": 3,
             "hour": 11,
             "professorId": 2,
             "room": "C20",
-            "subjectId": 1
+            "subjectId": 1,
+            "groupId" : 1 
         },
         id : null
     },
@@ -148,8 +155,15 @@ var secretaries = {
             },
             "subject": {
                 "id": Number,
-                "name": String
-            }
+                "name": String,
+                "credits" : Number
+            },
+            "professor" : {
+                "id": Number,
+                "firstName": String,
+                "lastName": String
+            },
+            "evaluationMethod": String
         },
         wrong : {
             "groupId": 100,
@@ -161,12 +175,15 @@ var secretaries = {
         },
         right : {
             "groupId": 2,
-            "subjectIds": [1,2,3,4]
+            "subjectId": 1,
+            "evaluationMethod": "100% Examen",
+            "professorId": 3
         }
     }
 };
 
 var admins = {
+    auth: null,
     secretaries: {
         types : {
             "ssn": String,
@@ -210,6 +227,7 @@ var admins = {
 };
 
 var students = {
+    auth: null,
     id : null
 };
 
@@ -230,36 +248,92 @@ frisby.create('Logging in with fake user')
     .expectStatus(400)
 .toss();
 
+// Log in as admin
 frisby.create('Logging in with admin credentials')
     .post(host + '/login',
     {
         "password": admin.password,
         "username": admin.username
-    }, {json:true})
+    }, 
+    {json:true})
     .expectStatus(200)
+    .afterJSON(function(json){
+        // Should succesfully login, now grab the Auth token
+        admins.auth = json.account.authorization;
+
+        // Test all admin functionalities, now that we are logged in
+        genericItemVerification(
+            'secretaries','/admins/',
+            admins.secretaries, 
+            admins.auth
+        );
+    })
 .toss();
 
-var genericItemVerification = function (module, path, item) {
+// Log in as secretary
+frisby.create('Logging in with secretary credentials')
+    .post(host + '/login',
+    {
+        "password": secretary.password,
+        "username": secretary.username
+    }, 
+    {json:true})
+    .expectStatus(200)
+    .afterJSON(function(json){
+        // Should succesfully login, now grab the Auth token
+        secretaries.auth = json.account.authorization;
+
+        // Test all secretary functionalities, now that we are logged in
+        genericItemVerification(
+            'professors','/secretaries/',
+            secretaries.professors,
+            secretaries.auth
+        );
+
+        genericItemVerification(
+            'students','/secretaries/',
+            secretaries.students,
+            secretaries.auth
+        );
+
+        genericItemVerification(
+            'educationPlans','/secretaries/',
+            secretaries.education_plan,
+            secretaries.auth
+        );
+
+        scheduleVerification(
+            secretaries.auth,
+            secretaries.schedules
+        );
+    })
+.toss();
+
+
+var genericItemVerification = function (module, path, item, auth) {
     frisby.create('Getting all ' + module)
         .get(host + path + module)
+        .addHeader('Authorization', auth)
         .expectJSONTypes('*',
         item.types)
-    .toss();
-
-    // Login-controller
+    .toss(); 
+    
     frisby.create('Inserting fake item in ' + module)
+        .addHeader('Authorization', auth)
         .post(host + path + module,
         item.wrong, {json:true})
         .expectStatus(400)
     .toss();
 
     frisby.create('Inserting proper item in ' + module)
+        .addHeader('Authorization', auth)
         .post(host + path + module,
         item.right, {json:true})
         .expectStatus(201)
     .toss();
 
     frisby.create('Getting all items to check for the last one we inserted ' + module)
+        .addHeader('Authorization', auth)
         .get(host + path + module)
         .afterJSON(function(json){
             item.id = json[json.length-1].id;
@@ -268,46 +342,89 @@ var genericItemVerification = function (module, path, item) {
             // REDO AFTER 
             if (module !== 'educationPlans') {
                 frisby.create('Grabbing our item from ' + module)
+                    .addHeader('Authorization', auth)
                     .get(host + path + module + '/' + item.id)
                     .expectStatus(200)
                 .toss();
             }
 
             frisby.create('Deleting our item')
+                .addHeader('Authorization', auth)
                 .delete(host + path + module + '/' + item.id)
                 .expectStatus(200)
             .toss();
         })
     .toss();
-} 
+}
 
-// Test secretaries controllers
-genericItemVerification('professors'    , '/secretaries/'   , secretaries.professors);
-genericItemVerification('schedules'     , '/secretaries/'   , secretaries.schedules);
-genericItemVerification('students'      , '/secretaries/'   , secretaries.students);
-genericItemVerification('secretaries'   , '/admins/'        , admins.secretaries);
-genericItemVerification('educationPlans', '/secretaries/'   , secretaries.education_plan);
+var scheduleVerification = function (auth, item) {
+    frisby.create('Getting all schedules')
+        .get(host + '/schedules')
+        .expectJSONTypes('*',
+        item.types)
+    .toss(); 
+    
+    frisby.create('Inserting fake item in schedules')
+        .addHeader('Authorization', auth)
+        .post(host + '/secretaries/schedules',
+        item.wrong, {json:true})
+        .expectStatus(400)
+    .toss();
 
-// Student controller
-frisby.create('Getting one student for a test')
-    .get(host + '/secretaries/students')
-    .expectStatus(200)
-    .afterJSON(function(json){
-        if (json.length > 0) {
-            students.id = json[0].id;
+    frisby.create('Inserting proper item in schedules')
+        .addHeader('Authorization', auth)
+        .post(host + '/secretaries/schedules',
+        item.right, {json:true})
+        .expectStatus(201)
+    .toss();
 
-            frisby.create('Getting selected user\'s grades')
-                .get(host + '/students/' + students.id + '/grades')
+    frisby.create('Getting all items to check for the last one we inserted ' + module)
+        .get(host + '/schedules')
+        .afterJSON(function(json){
+            item.id = json[json.length-1].id;
+
+            // TODO : TEMPORARY FIX
+            // REDO AFTER 
+            if (module !== 'educationPlans') {
+                frisby.create('Grabbing our item from ' + module)
+                    .addHeader('Authorization', auth)
+                    .get(host + '/secretaries/schedules/' + item.id)
+                    .expectStatus(200)
+                .toss();
+            }
+
+            frisby.create('Deleting our item')
+                .addHeader('Authorization', auth)
+                .delete(host + '/secretaries/schedules/' + item.id)
                 .expectStatus(200)
             .toss();
+        })
+    .toss();
+}
 
-            frisby.create('Getting selected user\'s info')
-                .get(host + '/students/' + students.id + '/info')
-                .expectStatus(200)
-            .toss();
-        }
-    })
-.toss();
+/*
 
+var studentVerification = function(auth) {
+    // Student controller
+    frisby.create('Getting one student for a test')
+        .get(host + '/secretaries/students')
+        .expectStatus(200)
+        .afterJSON(function(json){
+            if (json.length > 0) {
+                students.id = json[0].id;
 
+                frisby.create('Getting selected user\'s grades')
+                    .get(host + '/students/' + students.id + '/grades')
+                    .expectStatus(200)
+                .toss();
 
+                frisby.create('Getting selected user\'s info')
+                    .get(host + '/students/' + students.id + '/info')
+                    .expectStatus(200)
+                .toss();
+            }
+        })
+    .toss();
+}
+
+*/
